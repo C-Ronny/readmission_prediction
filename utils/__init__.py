22e385a91de43057751959d2ee6fb29651f4946f
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-12-24 22:23:11 +0000

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,14 +3,13 @@ Utility functions for Diabetes Readmission Prediction App
 """
 
 from .model_loader import load_models, get_feature_names
-from .preprocessor import preprocess_input, create_feature_vector
+from .preprocessor import preprocess_input
 from .predictor import predict_readmission, get_risk_message
 
 __all__ = [
     'load_models',
     'get_feature_names',
     'preprocess_input',
-    'create_feature_vector',
     'predict_readmission',
     'get_risk_message'
 ]
